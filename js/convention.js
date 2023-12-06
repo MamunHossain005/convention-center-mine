8362da6bcea81a0c75ea759dd8ef2807ef08fa7a
Md-Mamun-Hossain <hossainmamun624@gmail.com>
2023-12-07 00:37:57 +0600

    final commit

---
+++ js/convention.js
@@ -0,0 +1,20 @@
+// toast trigger
+const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+const toastTrigger1 = document.getElementById('liveToastBtn1')
+const toastLiveExample1 = document.getElementById('liveToast1')
+
+if (toastTrigger1) {
+  const toastBootstrap1 = bootstrap.Toast.getOrCreateInstance(toastLiveExample1)
+  toastTrigger1.addEventListener('click', () => {
+    toastBootstrap1.show()
+  })
+}
